64822a6df06ffd877615620e1294e47224d135e9
lifengyao <1412697557@qq.com>
2023-12-25 21:38:04 +0800

    some changes

--- themes/oranges/source/js/uptime.js
+++ themes/oranges/source/js/uptime.js
@@ -1,8 +1,15 @@
 function uptime() {
     const currentTime = new Date().getTime();
+    const thisYear = new Date().getFullYear();
     const startDate = new Date('2022-07-26 18:00:00').getTime();
     const timeDifference = currentTime - startDate;
     const daysRunning = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-    return daysRunning;
+    return {daysRunning, thisYear};
 }
+
+let e = document.querySelector('#stats-days')
+let y = document.querySelector('#year-copy')
+let x = uptime()
+e.innerHTML = `This site has been running for ${x.daysRunning} days.`
+y.innerHTML = `© ${x.thisYear} Jason Lee. All Rights Reserved.`
