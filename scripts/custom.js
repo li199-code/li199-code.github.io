e22bdd7d6f04b4690ed926bf3b84bddce7d4511f
Jason Lee <67300063+li199-code@users.noreply.github.com>
2024-06-04 21:56:22 +0800

    header: sitemap and og

--- scripts/custom.js
+++ scripts/custom.js
@@ -2,3 +2,23 @@ hexo.extend.injector.register('body_end', `
   <script src="/js/custom.js"></script>
   <link rel="stylesheet" href="/css/custom.css">
 `)
+
+hexo.extend.filter.register('after_render:html', function (data) {
+  // 定义要插入的 sitemap header 信息
+  const sitemapHeaderContent = `
+    <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" />
+  `;
+
+  const opengraphHeaderContent = `
+    <meta property="og:title" content="小 Lee 说">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="/images/avatar-removebg-preview.png">
+    <meta property="og:url" content="https://blog.jasonleehere.com">
+    <meta property="og:description" content="快乐在于折腾，快乐是自己给的！">
+  `;
+
+  // 将 sitemap header 内容插入到 <head> 标签内的最开始
+  data = data.replace(/<head>/, `<head>${sitemapHeaderContent}${opengraphHeaderContent}`);
+  
+  return data;
+});
